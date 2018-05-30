04d36fd9791018b3ab741f5c471944f510aca029
Andrew Henry <andrew_henry@fcp.biz>
2018-05-30 07:59:11 -0400

    Iinitial app commit for webapps. cContains just placeholder for Admin Console right now.

---
+++ source/WebApps/WebApps/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Windows;
+
+namespace WebApps
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // gets current view
+            string view = (Application.Current as App).WebView;
+
+            string path = view.Replace(' ', '_').ToLower();
+
+            // call browser class 
+            var browser = new Browser();
+            browserWin.Navigate(browser.CreateURL(Path.Combine(@"file://{0}", @"../../../../Web/" + path + "/index.html")));
+            
+            //relative pathing v.1
+            //browserWin.Navigate(browser.CreateURL(@"file://{0}/"+path+"/index.html"));
+            
+            this.Icon = browser.GetIcon(view);
+            this.Title = view;
+        }
+    }
+}
